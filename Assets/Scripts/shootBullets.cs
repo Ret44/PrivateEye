f91fdfc9419d3d9f9c049cb4d8905ac84c1c993e
Marek <marek.baczynski@blue-brick.com>
2014-02-15 17:18:40 +0100

    ship, main script and images

---
+++ Assets/Scripts/shootBullets.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootBullets : MonoBehaviour {
+
+	//lazy loading
+	public GameObject blastPrefab;
+
+	//movement config
+	private float vx = 0f;
+	private float vy = 0f;
+	private float acc = 1.3f;
+	private float damp = 0.76f;
+	private float shootDelay = 0.4f;
+	
+	//frame config
+//	private float boxTop = 5f;
+//	private float boxRight = 11f;
+//	private float boxDown = -5f;
+//	private float boxLeft = -11f;
+
+
+
+	//actually stuff
+	float lastShotTime = 0f;
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		vx += Input.GetAxis("Horizontal") * acc * Time.deltaTime;
+		vx *= damp;
+
+		vy += Input.GetAxis("Vertical") * acc * Time.deltaTime;
+		vy *= damp;
+
+		transform.position += new Vector3(vx, vy, 0f);
+
+		if(Input.GetKey(KeyCode.Space)){
+			if(Time.time > lastShotTime + shootDelay){
+				shoot ();
+			}
+		}
+
+	}
+
+
+	void shoot(){
+		//Instantiate(blastPrefab, transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/blast.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
